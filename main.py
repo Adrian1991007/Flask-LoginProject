0489d441591a379841aca4dfd81d24a2adb740e8
Gherasim Daniel Adrian <adriangherasim1@gmail.com>
2021-03-02 12:03:21 +0200

    produs intermediar

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, redirect, url_for
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('login.html')
+
+
+@app.route('/register')
+def new_prifile():
+    return render_template('register.html')
+
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
